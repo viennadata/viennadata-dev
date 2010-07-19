d4628a97fa555fcd74f64dabf85f5330365176d4
Karl Rupp <rupp@pcrupp.(none)>
2010-07-19 20:08:46 +0200

    Designed new interface as comment in main.cpp

--- main.cpp
+++ main.cpp
@@ -9,11 +9,23 @@
 //#include "QuantityManager/quantitymanager.h"
 #include "testbench.h"
 
+/*
+remove  <key_type, value_type>(key)(obj);
+data    <key_type, value_type>(key)(obj) = value;
+value = data     <key_type, value_type>(key)(obj);
+transfer<key_type, value_type>(key)(obj_src, obj_dest); //think about it!
+transfer<key_type, value_type>()(obj_src, obj_dest);
+
+transfer<key_type, all       >()(obj_src, obj_dest);
+transfer<all,      value_type>()(obj_src, obj_dest);
+transfer<all,      all       >()(obj_src, obj_dest);
+*/
+
 int main(int argc, char *argv[])
 {
 
   testBasicOperations();
   //testStorageKeyWrappers();
-  
+
   return EXIT_SUCCESS;
 }
