a43387681e7a7f4848f18924b649538e4e679267
Karl Rupp <rupp@iue.tuwien.ac.at>
2013-06-19 17:05:12 -0500

    Fixed nightly build test system to run with the new CMake build system.

--- tests/run_regression.sh
+++ tests/run_regression.sh
@@ -25,7 +25,7 @@ if [ "$INPUT" != "" ]; then
       echo ""
       echo "regression result is available here:"
       echo "----------------------------------------------"
-      echo "http://my.cdash.org/index.php?project=ViennaCL"
+      echo "http://my.cdash.org/index.php?project=ViennaData"
       echo "----------------------------------------------"
       echo ""
    else 
