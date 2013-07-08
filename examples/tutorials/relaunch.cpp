f0d93cb3ac6559c71b4a30ddeec54622facf9a83
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-07-08 13:05:25 +0200

    added some comments

--- examples/tutorials/relaunch.cpp
+++ examples/tutorials/relaunch.cpp
@@ -41,10 +41,9 @@ struct test_struct2
 
 
 struct my_trivial_key
-{
-    bool operator=(const my_trivial_key &) const { return true; }
-};
+{};
 
+// When using a trivial key operator< MUST return false
 bool operator<(my_trivial_key const &, my_trivial_key const &)
 {
     return false;
@@ -54,13 +53,21 @@ bool operator<(my_trivial_key const &, my_trivial_key const &)
 
 int main()
 {
+  
+  // create a custom container configuration
+  // configuration of default_tag is used for every value-type, key-type, access-type tripple not explicit configured
+  // storage<> has default std::map with pointer access
+  // the custom config below has default std::map with pointer access and for class test_struct with key-type int and value-type double std::deque with id access
+  // when using id access the access-type has to provide ::id_type and ::id()
   typedef viennameta::make_typemap<
     viennadata::default_tag, viennameta::static_pair<viennadata::std_map_tag, viennadata::pointer_access_tag>,
     viennameta::static_pair< test_struct, viennameta::static_pair<int, double> >, viennameta::static_pair<viennadata::std_deque_tag, viennadata::id_access_tag>
   >::type container_config;
 
+  // create a storage using ur custom configuration
   viennadata::storage<container_config> my_storage;
 
+  // create 2 containers for testing
   std::vector<test_struct> elements;
   std::vector<test_struct2> elements2;
 
@@ -73,6 +80,7 @@ int main()
   }
 
 
+  // using viennadata to fill meta data for test containers
   {
     double val = 0.0;
     for (std::vector<test_struct>::iterator it = elements.begin(); it != elements.end(); ++it, val += 1.0)
@@ -85,45 +93,75 @@ int main()
       viennadata::access<int, double>(my_storage, 0, *it) = val;
   }
 
-  test_struct2 tmp(10);
-
+  
+  // query the underlying container for key-type double, value-type double and access-type test_struct2
+  // Syntax: viennadata::result_of::container_type< STORAGE_TYPE, KEY_TYPE, VALUE_TYPE, ACCESS_TYPE>::type
+  // Syntax: viennadata::container<KEY_TYPE, VALUE_TYPE, ACCESS_TYPE>(storage, key);
   viennadata::result_of::container_type< viennadata::storage<container_config>, double, double, test_struct2>::type & c =
       viennadata::container<double, double, test_struct2>(my_storage, 0);
 
 
+  // erasing value from the storage
+  // beware: erased from dense container might not actually erase a value
   std::cout << "\nErase Test\n" << std::endl;
+  
+  test_struct2 tmp(10);
 
+  // add with temporary data for tmp
   viennadata::access< double, double >(my_storage, 0, tmp) = 1;
   viennadata::access< int, double >(my_storage, 0, tmp) = 2;
   viennadata::access< double, int >(my_storage, 0, tmp) = 3;
 
+  // finding the data -> should be available (point is not NULL)
+  // Syntax: viennadata::find<KEY_TYPE, VALUE_TYPE>(storage, key, to_access)
   std::cout << viennadata::find<double, double>(my_storage, 0, tmp) << std::endl;
   std::cout << viennadata::find<int, double>(my_storage, 0, tmp) << std::endl;
   std::cout << viennadata::find<double, int>(my_storage, 0, tmp) << std::endl;
 
-//         viennadata::erase<viennadata::all, viennadata::all>(my_storage, tmp);
-//         viennadata::erase<viennadata::all, int>(my_storage, tmp);
 
+
+  // query container size (should be 1)
   std::cout << "Container size = " << c.size() << std::endl;
+  
+  // erasing data for tmp, key-type double, all value types
+  // Syntax: viennadata::erase<KEY_TYPE, VALUE_TYPE>(storage, key, to_access)
+  
   viennadata::erase<double, viennadata::all>(my_storage, 0, tmp);
+  
+  // some other erase tests
+  // KEY_TYPE and VALUE_TYPE can be viennadata::all if all keys and key_types/all value type should be erased
+  // viennadata::erase<viennadata::all, viennadata::all>(my_storage, tmp); // no key needed
+  // viennadata::erase<viennadata::all, int>(my_storage, tmp); // no key needed
 
+  // first and third should be NULL now
   std::cout << viennadata::find<double, double>(my_storage, 0, tmp) << std::endl;
   std::cout << viennadata::find<int, double>(my_storage, 0, tmp) << std::endl;
   std::cout << viennadata::find<double, int>(my_storage, 0, tmp) << std::endl;
 
-
+  // query container size (should be 0)
   std::cout << "Container size = " << c.size() << std::endl;
 
 
+  // Some testing with Accessors
   std::cout << "\nAccessor Test\n" << std::endl;
 
+  // query an accessor for key-type double, value-type double and access-type test_struct2
+  // Syntax: viennadata::result_of::accessor< STORAGE_TYPE, KEY_TYPE, VALUE_TYPE, ACCESS_TYPE>::type
+  // Syntax: viennadata::accessor<KEY_TYPE, VALUE_TYPE, ACCESS_TYPE>(storage, key);
   viennadata::result_of::accessor<viennadata::storage<container_config>, double, double, test_struct2>::type
       accessor = viennadata::accessor<double, double, test_struct2>(my_storage, 0);
 
+  // simple accessor access
   accessor(tmp) = 3.14;
   std::cout << "Accessor: " << accessor(tmp) << std::endl;
+  
+  // find return Pointer
   std::cout << "Accessor find: " << accessor.find(tmp) << std::endl;
+  
+  // erase might not work properly on dense containers
   accessor.erase(tmp);
+  
+  // should ne NULL
   std::cout << "Accessor find: " << accessor.find(tmp) << std::endl;
 
 
@@ -135,20 +173,27 @@ int main()
   std::cout << viennadata::access<double, double>(my_storage, 0, tmp) << std::endl;
   std::cout << viennadata::access<int, double>(my_storage, 0, tmp) << std::endl;
   std::cout << viennadata::access<double, int>(my_storage, 0, tmp) << std::endl;
+  std::cout << std::endl;
 
   test_struct2 tmp1(11);
 
+  // Copy data
+  // Syntax: viennadata::copy<KEY_TYPE, VALUE_TYPE>(storage, key, source_access, destination_access)
+  // KEY_TYPE and/or VALUE_TYPE can be viennadata::all
   viennadata::copy<double, viennadata::all>(my_storage, 0, tmp, tmp1);
 
   std::cout << viennadata::access<double, double>(my_storage, 0, tmp) << std::endl;
   std::cout << viennadata::access<int, double>(my_storage, 0, tmp) << std::endl;
   std::cout << viennadata::access<double, int>(my_storage, 0, tmp) << std::endl;
+  std::cout << std::endl;
 
+  // the first and the third should have been copied
   std::cout << viennadata::access<double, double>(my_storage, 0, tmp1) << std::endl;
   std::cout << viennadata::access<int, double>(my_storage, 0, tmp1) << std::endl;
   std::cout << viennadata::access<double, int>(my_storage, 0, tmp1) << std::endl;
 
 
+  // Example using a trivial key (something similar to type-based dispatching) 
   std::cout << "\nTrivial Key\n" << std::endl;
   viennadata::access< my_trivial_key, double >(my_storage, my_trivial_key(), tmp) = 42;
   std::cout << "my trivial key = " << viennadata::access< my_trivial_key, double >(my_storage, my_trivial_key(), tmp) << std::endl;
